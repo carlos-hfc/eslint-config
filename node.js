c8d3ee4aa5e039019f826a262ac6da4d46f4f496
Carlos Faustino <chfcchfc96@gmail.com>
2025-06-27 12:48:07 -0300

    feat: update linting

--- node.js
+++ node.js
@@ -1,31 +1,55 @@
-module.exports = {
-  env: {
-    node: true,
-    es2021: true,
-  },
-  extends: [
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default defineConfig([{
+  languageOptions: {
+    globals: {
+      ...globals.node,
+    },
+
+    parser: tsParser,
     ecmaVersion: "latest",
     sourceType: "module",
+    parserOptions: {},
   },
-  plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports"],
+
+  extends: compat.extends(
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ),
+
+  plugins: {
+    "@typescript-eslint": typescriptEslint,
+    "simple-import-sort": simpleImportSort,
+    "unused-imports": unusedImports,
+  },
+
   rules: {
-    "prettier/prettier": [
-      "warn",
-      {
-        arrowParens: "avoid",
-        endOfLine: "auto",
-        printWidth: 80,
-        semi: false,
-        singleAttributePerLine: true,
-        tabWidth: 2,
-      },
-    ],
+    "prettier/prettier": ["warn", {
+      arrowParens: "avoid",
+      endOfLine: "auto",
+      printWidth: 80,
+      semi: false,
+      singleAttributePerLine: true,
+      tabWidth: 2,
+    }],
+
     "no-unused-vars": "off",
     "no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
@@ -33,9 +57,10 @@ module.exports = {
     "unused-imports/no-unused-imports": "warn",
     "simple-import-sort/imports": "warn",
   },
+
   settings: {
     "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+      [import("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
   },
-}
+}]);
--- react.js
+++ react.js
@@ -1,72 +1,109 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "standard",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: [
-    "react",
-    "jsx-a11y",
-    "@typescript-eslint",
-    "simple-import-sort",
-    "unused-imports",
-  ],
-  rules: {
-    "prettier/prettier": [
-      "warn",
-      {
-        arrowParens: "avoid",
-        endOfLine: "auto",
-        printWidth: 80,
-        semi: false,
-        singleAttributePerLine: true,
-        tabWidth: 2,
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+
+import { fixupConfigRules, fixupPluginRules } from "@eslint/compat"
+import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import { defineConfig, globalIgnores } from "eslint/config"
+import jsxA11Y from "eslint-plugin-jsx-a11y"
+import react from "eslint-plugin-react"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import unusedImports from "eslint-plugin-unused-imports"
+import globals from "globals"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
       },
-    ],
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "jsx-a11y/alt-text": [
-      "warn",
-      {
-        elements: ["img"],
-        img: ["Image"],
+
+      parser: tsParser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
-    ],
-    "jsx-a11y/aria-props": "warn",
-    "jsx-a11y/aria-proptypes": "warn",
-    "jsx-a11y/aria-unsupported-elements": "warn",
-    "jsx-a11y/role-has-required-aria-props": "warn",
-    "jsx-a11y/role-supports-aria-props": "warn",
-    "react/no-unknown-property": "error",
-    "no-unused-vars": "off",
-    "no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "unused-imports/no-unused-imports": "warn",
-    "simple-import-sort/imports": "warn",
-  },
-  settings: {
-    react: {
-      version: "detect",
     },
-    "import/parsers": {
-      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+      ),
+    ),
+
+    plugins: {
+      react: fixupPluginRules(react),
+      "jsx-a11y": jsxA11Y,
+      "@typescript-eslint": fixupPluginRules(typescriptEslint),
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+    },
+
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "avoid",
+          endOfLine: "auto",
+          printWidth: 80,
+          semi: false,
+          singleAttributePerLine: true,
+          tabWidth: 2,
+        },
+      ],
+
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      "jsx-a11y/alt-text": [
+        "warn",
+        {
+          elements: ["img"],
+          img: ["Image"],
+        },
+      ],
+
+      "jsx-a11y/aria-props": "warn",
+      "jsx-a11y/aria-proptypes": "warn",
+      "jsx-a11y/aria-unsupported-elements": "warn",
+      "jsx-a11y/role-has-required-aria-props": "warn",
+      "jsx-a11y/role-supports-aria-props": "warn",
+      "react/no-unknown-property": "error",
+      "no-unused-vars": "off",
+      "no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "unused-imports/no-unused-imports": "warn",
+      "simple-import-sort/imports": "warn",
+    },
+
+    settings: {
+      react: {
+        version: "detect",
+      },
+
+      "import/parsers": {
+        [import("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
+      },
     },
   },
-  ignorePatterns: ["node_modules"],
-}
+  globalIgnores(["**/node_modules"]),
+])
